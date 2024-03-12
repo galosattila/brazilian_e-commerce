f890179fc387973e7e7c5419e487413f6aedbb7d
galosattila <galosati@gmail.com>
2024-03-12 16:07:50 +0100

    added error handling

--- core/data_loader.py
+++ core/data_loader.py
@@ -2,10 +2,12 @@ import pandas as pd
 import sys
 import os
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
-
 
 def load_data_from_csv(file_path: str) -> pd.DataFrame:
-    df = pd.read_csv(file_path)
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError as e:
+        print(f"Error: Dataset file not found: {file_path}")
+        exit(1)
+
     return df
